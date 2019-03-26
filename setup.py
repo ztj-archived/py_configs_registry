01a9fd05ae978b7c8d9016c8af5be1c0f84c8751
zhangtianjie <715751607@qq.com>
2019-03-26 11:38:29 +0800

    发布版本 0.0.2

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ except:
 
 setup(
     name='py_configs_registry',
-    version='0.0.1',
+    version='0.0.2',
     description='python registry package',
     long_description=long_description,
+    long_description_content_type="text/markdown",
+
     py_modules=["registry"],
 
     url='http://github.com/ztj1993/py_configs_registry',
